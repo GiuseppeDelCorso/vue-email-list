be1063e1e5391e412d4bffb2a9c5f1f0266f4390
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-20 16:00:01 +0100

    sv

--- main.js
+++ main.js
@@ -3,32 +3,36 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
-            mail: "",
-
-
-
+            mail: [],
         }
     },
-
+    
+    
     mounted() {
-
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
-           
-            if(resp.data.success){
-                
-            console.log(resp.data.response)
-            this.mail = resp.data.response
-
         
+        for (let index = 0; index < 10; index++) {
+            
+            
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
+                if(resp.data.success){
+                    
+                    console.log(resp.data.response)
+                    
+                    this.mail.push (resp.data.response)
+                    
+                    
+                }
+                
+            })
+            
+            
+            
         }
         
-        });
-
-
-    }
-
-
-
-
+        
+    },
+    
+    
+    
+    
 }).mount('#app')
